8adce6261d3dbf03358b9ed0e0f91922d5b68d48
ceb@ <42058726+ceb-at@users.noreply.github.com>
2019-02-28 21:31:09 +0100

    Update leapyeartestpython.py

--- leapyeartestpython.py
+++ leapyeartestpython.py
@@ -4,22 +4,47 @@ print("############################")
 print("leap year test")
 print("############################")
 
-year = input("Type a year:")
-year = int(year)
-leap_year = False
-
-if year%400 == 0:
-    leap_year = True
-elif year%100 == 0:
-      leap_year = False
-elif year%4 == 0:
-     leap_year = True
-else:
+def leapyeartest():
     leap_year = False
+    year = input("Type a year:")
+    year = int(year)
+    if year%400 == 0:
+        leap_year = True
+    elif year%100 == 0:
+        leap_year = False
+    elif year%4 == 0:
+        leap_year = True
+    else:
+        leap_year = False
+    if leap_year == True:
+        print("This year is a leap year!")
+    else:
+        print("This year is not a leap year!")
 
-if leap_year == True:
-     print("This year is a leap year!")
-else:
-    print("This year is not a leap year!")
+def want_to_start():
+    start = False
+    ask_start = 1
+    ask_start = input("Want you start ? Yes = 1 | No = 0")
+    if type(ask_start) == int or float:
+        ask_start = float(ask_start)
+        if float(ask_start) == 1:
+            start = True
+        elif float(ask_start) == 0:
+            start = False
+    else:
+        print("ERROR-1")
+        want_to_start()
+    if start == True:
+        leapyeartest()
+    elif start == Fal
+    se:
+        want_to_start()
+    else:
+        print("ERROR-2")
+    want_to_start()
+
+want_to_start()
+
+
 
 #by ceb@
